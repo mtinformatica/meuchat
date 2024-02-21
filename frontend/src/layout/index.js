d03612f4dcffb16ffe1ec4addf2fc0f61962e49d
Magno Pereira <suporte@mtinfor.com.br>
2024-02-20 21:57:57 -0300

    up

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -162,7 +162,7 @@ const useStyles = makeStyles((theme) => ({
     // color: theme.barraSuperior.secondary.main,
   },
   logo: {
-    width: "10%",
+    width: "80%",
     height: "auto",
     maxWidth: 180,
     [theme.breakpoints.down("sm")]: {
